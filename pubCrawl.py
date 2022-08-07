494126663cb60c6ef25207c6a969e981ee3f0b4f
John Weis <wyrm.water@gmail.com>
2022-08-07 14:20:50 -0400

    Function updating
    Worked on a few functions to get them working in a simple state

--- pubCrawl.py
+++ pubCrawl.py
@@ -6,6 +6,7 @@
 
 #*IMPORT BLOCK BEGIN*
 from bs4 import BeautifulSoup as bs
+import random
 import requests
 import re 
 import sys
@@ -16,9 +17,14 @@ import sys
 #**STUB BLOCK BEGIN**
 #***FUTURE VERSION BLOCK BEGIN***
 
+#!STUB(Version 2.0 Feature): Function for attempting to clean up user's input & parsing it properly for future connection/handling.
+def userInputParser(providedURL):
+	return providedURL
+
 #STUB(Version 2.0 Feature): Function for managing & orchestrating the recursive function of user defined depths of scraping on found resources.
-def recursiveDepthManager():
-	pass
+def recursiveDepthManager(intDepth = 1):
+	if(intDepth == 1):
+		pass
 
 #STUB(Version 3.0 Feature): Function for integrating API calls on malicious site checkers(Brightcloud, Virustotal, etc) to automatically check provided URLs for malicious activity/poor community reputation.
 def siteRepCheck():
@@ -30,41 +36,41 @@ def siteRepCheck():
 def fileNamer():
 	pass
 
-#!STUB(Incomplete): Function for accepting & parsing user input from the console line.  Expected input is from the sys.argv format that comes from the console.  
-def userInputHandler(systemInput):
-	defaultURL = "http://scanme.nmap.org/"
-	print(len(sys.argv))
-	if(len(systemInput) <= 1):
-		providedURL = defaultURL
-	else:
-		providedURL = sys.argv[1]
-
-	return(providedURL)
-
-#!STUB(Incomplete): Function for attempting to clean up user's input & parsing it properly for future connection/handling.
-def userInputParser(providedURL):
-	pass
-
-#!STUB(Incomplete): Function for negotiating & connecting to user supplied URL.
-def siteConnector():
-	pass
-
 #!STUB(Incomplete): Function for scraping the URL provided by the user for usable information that is stored in the HTML(via hrefs).
-def siteScraper():
-	pass
+def siteScraper(providedHTML):
+	for link in providedHTML.find_all(attrs={'href': re.compile("^https?://")}):
+		print("\'href\': " + link.get('href'))
 
 #!STUB(Incomplete): Function for parsing the data that is found from the siteScraper & saving it to a txt file.
 def dataParser():
 	pass
 
-#!STUB(Incomplete): main function: Function for the primary orchestration & running of the code
-def main():
-	workingURL = (userInputHandler(sys.argv))
-	print(workingURL)
-	print(type(workingURL))
 
 #***INCOMPLETE BLOCK BEGIN***
 #**STUB BLOCK END**
+
+#Function for negotiating & connecting to user supplied URL.
+def siteConnector(workingURL):
+	response = requests.get(workingURL)
+	return(response.text)
+
+#Function for accepting & parsing user input from the console line.  Expected input is from the sys.argv format that comes from the console.  
+def userInputHandler(systemInput):
+	defaultURL = "http://scanme.nmap.org/"
+	if(len(systemInput) <= 1):
+		cleanedURL = defaultURL
+	else:
+		providedURL = sys.argv[1]
+		cleanedURL = userInputParser(providedURL)
+	return(cleanedURL)
+
+#main function: Function for the primary orchestration & running of the code
+def main():
+	workingURL = userInputHandler(sys.argv)
+	targetResponse = siteConnector(workingURL)
+	siteHTML = bs(targetResponse, 'html.parser')
+	urlList = siteScraper(siteHTML)
+
 #*FUNCTION BLOCK END*
 
 if __name__ == '__main__':
