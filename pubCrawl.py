a6b571f6d7a2aecc3251f569959d3c4f8e628ab6
John Weis <wyrm.water@gmail.com>
2022-08-22 11:47:33 -0400

    fileReader() create; dataPoolSorter() create; fileNamer() update;
    Created fileReader() function for accepting files, reading the file, & returning the data as a str data pool
    Created dataPoolSorter() lambda for accepting str data pools & returning them as dictionaries
    updated fileNamer() function to get all those gross adjective & noun dictionaries out of the function & put in a more appropriate .txt file

--- pubCrawl.py
+++ pubCrawl.py
@@ -1,4 +1,4 @@
-#! python3 (3.9.5)
+#! python3
 # Version: 1.0
 # Developed Platform: Ubuntu Linux 20.04(Ubuntu 20.04.4 LTS)
 # Operation Platform: Debian Linux instances
@@ -6,6 +6,7 @@
 
 #*IMPORT BLOCK BEGIN*
 from bs4 import BeautifulSoup as bs
+import json
 import random
 import requests
 import re
@@ -14,6 +15,15 @@ import sys
 import datetime
 
 #*IMPORT BLOCK END*
+
+#*LAMBDA BLOCK BEGIN*
+#**INCOMPLETE BLOCK BEGIN**
+
+#!INCOMPLETE(Being Tested): Lambda designed to read str pool of data & output it as a dictionary.  Expected output: dict.
+dataPoolSorter = lambda data : json.loads(data)
+
+#**INCOMPLETE BLOCK END**
+#*LAMBDA BLOCK END*
 
 #*FUNCTION BLOCK BEGIN*
 #**STUB BLOCK BEGIN**
@@ -37,6 +47,15 @@ def siteRepCheck():
 
 #***FUTURE VERSION BLOCK END***
 #***INCOMPLETE BLOCK BEGIN***
+
+#!INCOMPLETE(Being Tested): Function for reading exterior files & returning their value to requestor. Expected output: str.
+def fileReader(fullFileName):
+	try:
+		with open(fullFileName) as file:
+			data = file.read()
+		return data
+	except Exception as e:
+		logHandler(e, 'fileReader', 'ERROR')
 
 #!STUB(Being Tested): Function for gathering error information & recording it to a central location.
 def logHandler(loggingData, functionSource, logType = 'INFORMATION'):
@@ -98,37 +117,16 @@ def userInputHandler(systemInput):
 
 #Feature for generating a unique filename for each of the pubCrawl outputs.
 def fileNamer():
-	adjectives = {"Environment":["desert", "tundra", "mountain", "space", "field", "urban"],
-"StealthCunning":["hidden", "covert", "uncanny", "scheming", "decisive", "untouchable", "stalking"],
-"Volatility":["rowdy", "dangerous", "explosive", "threatening", "warring", "deadly", "killer", "insane", "wild"],
-"NegativeConnotation":["bad", "unnecessary", "unknown", "unexpected", "waning"],
-"Colors":["red", "orange", "yellow", "green", "blue", "violet"],
-"Adjectives":["draconic", "wireless", "spinning", "falling", "orbiting", "hunting", "chasing", "searching", "revealing", "flying", "destroyed", "inconceivable", "tarnished"]}
-	nouns = {"LargeCats":["panther", "wildcat", "tiger", "lion", "cheetah", "cougar", "leopard", "jaguar"],
-"Snakes":["anaconda", "viper", "cottonmouth", "python", "boa", "sidewinder", "cobra"],
-"Birds":["falcon", "eagle", "hawk", "owl", "osprey", "vulture", "petrel", "sparrow", "finch", "cardinal", "dove", "pigeon", "heron", "gull"],
-"Insects":["tick", "beetle", "fly", "cicada", "weevil", "ant", "dragonfly", "damselfly", "grasshopper", "flea", "mantis"],
-"Predators":["coyote", "wolf", "jackal", "bear", "stoat", "ferret", "weasel", "mink", "alligator", "crocodile", "badger", "wolverine", "hyena"],
-"Prey":["wildebeest", "gazelle", "zebra", "elk", "moose", "deer", "stag", "koala", "sloth", "quoll"],
-"Equines":["horse", "stallion", "foal", "colt", "mare", "yearling", "filly", "gelding", "mule", "donkey", "pony"],
-"MythicalCreatures":["mermaid", "unicorn", "fairy", "troll", "yeti", "pegasus", "griffin", "dragon"],
-"OrganicMaterials":[ "amber", "bone", "coral", "ivory", "jet", "nacre", "pearl", "obsidian", "glass"],
-"Gems":["agate", "beryl", "diamond", "opal", "ruby", "onyx", "sapphire", "emerald", "jade"],
-"Occupations":["nomad", "wizard", "cleric", "pilot", "captain", "commander", "general", "major", "admiral", "chef", "inspector", "sorcerer", "warlock", "fighter", "barbarian", "monk", "farmer", "soldier", "broker", "accountant", "auditor", "actor", "actress", "nurse", "doctor", "manager", "operator", "agent", "spy", "engineer", "carpenter", "cooper", "cobbler", "smith", "artist", "athlete", "technician", "officer", "supervisor", "controller", "attendant", "assembler", "specialist", "breeder", "caretaker", "trainer", "teacher", "tutor", "appraiser", "assessor", "arbitrator", "mediator", "councilor", "lawyer", "architect", "drafter", "recruiter", "recruit", "greenhorn", "novice", "journeyman", "archivist", "scientist", "researcher", "director", "producer", "therapist", "worker", "laborer", "astronomer", "marine", "grunt", "astronaut", "mechanic", "installer", "bellhop", "porter", "bailiff", "baker", "barber", "bartender", "clerk", "collector", "chemist", "physicists", "developer", "biologist", "bookkeeper", "mason", "analyst", "janitor", "driver", "planner", "cutter", "butcher", "buyer", "purchaser", "merchant", "trader", "mate", "cartographer", "cashier", "teller", "cook", "executive", "choreographer", "adjuster", "investigator", "clergyman", "pope", "cardinal", "bishop", "psychologist", "psychiatrist", "coordinator", "coach", "scout", "painter", "concierge", "miner", "tender", "coroner", "paramedic", "jailer", "messenger", "courier", "guard", "curator", "representative", "dancer", "performer", "musician", "administrator", "assistant", "salesman", "vendor", "editor", "advisor", "electrician", "etcher", "engraver", "extractor", "fisherman", "forester", "contractor", "rancher", "farmhand", "watchman", "weaver", "roofer", "wheelwright", "locksmith", "tanner", "grocer", "minstrel", "miller", "armorer", "barrister"],
-"Technology":["mainframe", "device", "motherboard", "network", "transistor", "packet", "robot", "android", "cyborg", "display", "battery", "memory", "disk", "computer", "server", "cartridge", "tape", "camera", "projector"],
-"SeaCreatures":["octopus", "lobster", "crab", "barnacle", "hammerhead", "orca", "piranha", "shark", "whale"],
-"Weather":["storm", "thunder", "lightning", "rain", "hail", "sun", "drought", "snow", "drizzle"],
-"Music":["piano", "keyboard", "guitar", "trumpet", "trombone", "flute", "cornet", "horn", "tuba", "clarinet", "saxophone", "piccolo", "violin", "harp", "cello", "drum", "organ", "banjo", "rhythm", "beat", "sound", "song"],
-"Tools":["lathe","mill","mask","screwdriver", "hammer", "pliers", "wrench", "saw", "axe", "crowbar", "file", "vise", "anvil", "scissors", "clamp", "planer", "mallet", "chisel", "level", "plumb", "rake", "shovel"],
-'Tableware':["fork", "spoon", "knife", "plate", "bowl", "dish", "mug", "cup", "shaker", "platter"],
-'Weapons':["dagger", "gun", "knife", "cannon", "mortar", "bola","boomerang","crossbow","longbow","bow","sling","spear","bayonet", "club", "dagger", "halberd", "lance", "pike", "quarterstaff", "sabre", "sword", "tomahawk", "grenade", "mine", "missile", "torpedo", "bazooka", "blunderbuss", "shotgun", "rifle", "pistol", "carbine", "revolver", "musket", "catapult", "ballista"],
-'Illness':["plague", "fever","smallpox","covid"],
-'Poison':["anthrax","ricin"],
-'Armor':["Aventail", "Bevor", "Coif", "Gorget", "Helmet", "Armet", "Barbute", "Bascinet", "Helm", "Morion", "Sallet", "Spangenhelm", "Visor", "Brigandine", "Codpiece", "Cuirass", "Fauld", "Gambeson", "Hauberk", "Jupon", "Pourpoint", "Ailette", "Besagew", "Couter", "Gauntlet", "Pauldron", "Rerebrace", "Spaulder", "Vambrace", "Chausses", "Cuisses", "Greaves", "Poleyn", "Sabaton", "Schynbald", "Tassets"],
-'Appliances':["kettle","fridge", "refrigerator", "microwave", "oven", "stove", "washer", "dryer", "vacuum", "freezer", "toaster", "blender", "pan", "steamer", "rack", "broom", "mop", "brush", "bucket", "rag", "towel", "napkin"],
-"MundaneObjects":["chain","ink","case","door"],
-"Nouns":["warning", "presence", "weapon", "player"]
-}
+	filePath = "./pubCrawl/fileNames/"
+	adjectiveFile = "adjectives.txt"
+	adjectiveFilePath = filePath + adjectiveFile
+	adjectiveDataPool = fileReader(adjectiveFilePath)
+	adjectives = dataPoolSorter(adjectiveDataPool)
+	nounFile = "nouns.txt"
+	nounFilePath = filePath + nounFile
+	nounDataPool = fileReader(nounFilePath)
+	nouns = dataPoolSorter(nounDataPool)
+
 	try:
 		adjectiveKey = random.choice(list(adjectives))
 		adjectivePool = adjectives[adjectiveKey]
